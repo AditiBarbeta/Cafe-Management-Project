658f5e5baa162bf7c3222531c2a35bf78db8a218
AditiBarbeta <125474932+AditiBarbeta@users.noreply.github.com>
2023-07-08 00:11:26 +0530

    Logic Files

---
+++ java.logic/Bill.java
@@ -0,0 +1,14 @@
+package group1.logic;
+
+public class Bill {
+
+	private double grandTotal;
+	OfferPolicy o = new OfferPolicy();
+	Order o3 = new Order();
+	
+	public double computeGrandTotal(double s, double d) {
+		this.grandTotal =s - (s * (d/100));
+		return this.grandTotal;
+	}
+	
+}
---
+++ java.logic/Cafe.java
@@ -0,0 +1,13 @@
+package group1.logic;
+
+public class Cafe {
+	private Menu menu;
+	private CustomerDetails customerDetails ;
+	private Table table;
+	
+	
+	public void displayName() {
+		System.out.println("----------------WELCOME TO THE DESTRUCTED CAFE----------------");
+	}
+
+}
